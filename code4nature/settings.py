1611e386cf74d84fa8803fe06a0b4ce5d6789f84
Pratham Satani <iamprathamsatani2002@gmail.com>
2024-06-26 00:24:11 +0530

    26th Jul

--- code4nature/settings.py
+++ code4nature/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +42,9 @@ INSTALLED_APPS = [
     'rest_framework',
     'krishimitra',
 ]
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
