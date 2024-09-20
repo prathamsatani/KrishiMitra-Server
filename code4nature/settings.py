7ae5adb0bd97908c7bf5c6ecc7893d5b7142040c
Pratham Satani <iamprathamsatani2002@gmail.com>
2024-09-20 20:18:56 +0000

    Add public folder for static files

--- code4nature/settings.py
+++ code4nature/settings.py
@@ -46,7 +46,10 @@ INSTALLED_APPS = [
 ]
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "public")
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
