fcdf1f9e1bcb2692c002fe141f7891c120972614
Pratham Satani <iamprathamsatani2002@gmail.com>
2024-09-21 22:26:00 +0530

    Update settings.py

--- code4nature/settings.py
+++ code4nature/settings.py
@@ -28,7 +28,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ['https://railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://krishimitra.up.railway.app']
 
 # Application definition
 
